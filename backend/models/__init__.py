bff4e3861d407fe21e139a6a2cdb676a55129c5e
xvibaan <xvibaan@gmail.com>
2026-09-12 22:41:08 +0530

    feat: register all database models

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,2 +1,5 @@
+# Import all models here so Alembic can detect them for migrations via Base.metadata
+
 from .user import User
-from .product import Product
+from .product import Product, ProductVariant, ProductKey
+from .order import Order, OrderItem
